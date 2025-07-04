d0ddc80cd9bf39243cc04eca0d5ada1c9875fc02
prajwal851 <prajwaldhital851.com>
2025-07-04 09:27:58 +0545

    Fix: Remove or correct Routes usage in App.tsx to resolve build error

--- restaurant_frontend/src/App.tsx
+++ restaurant_frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import PaymentSuccess from './pages/PaymentSuccess';
-
+import { Routes, Route } from 'react-router-dom';
 <Routes>
   <Route path="/payment/success" element={<PaymentSuccess />} />
 </Routes> 
